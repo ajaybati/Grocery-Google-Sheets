3ce641c0428998e6747203688f08f787cc2952a3
Ajay Bati <ajaybati@gmail.com>
2020-02-08 19:21:09 -0800

    adding file

---
+++ app/src/main/java/com/example/ajaybati/test/MainActivity.java
@@ -0,0 +1,342 @@
+package com.example.ajaybati.test;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+
+import com.example.ajaybati.test.adapter.MyArrayAdapter;
+import com.example.ajaybati.test.model.MyDataModel;
+import com.example.ajaybati.test.parser.JSONParser;
+import com.example.ajaybati.test.util.InternetConnection;
+import com.example.ajaybati.test.util.Keys;
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayList<String> list;
+    private ArrayAdapter adapter;
+    private String checked;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Fresco.initialize(this);
+
+
+
+        /**
+         * Array List for Binding Data from JSON to this List
+         */
+        list = new ArrayList<>();
+
+
+        /*
+         * Binding that List to Adapter
+         */
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_checked, list);
+
+        /**
+         * Getting List and Setting List Adapter
+         */
+        listView=(ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Snackbar.make(findViewById(R.id.parentLayout), list.get(position).getName() + " => " + list.get(position).getCountry(), Snackbar.LENGTH_LONG).show();
+                if(listView.isItemChecked(position)) {
+                    listView.setItemChecked(position, true);
+                    //checkedList.set(position,listView.getItemAtPosition(position).toString());
+                    checked=listView.getItemAtPosition(position).toString();
+                }else{
+                    listView.setItemChecked(position,false);
+                    checked="";
+                }
+                new SendRequest().execute();
+            }
+
+
+        });
+
+        /**
+         * Just to know onClick and Printing Hello Toast in Center.
+         */
+        Toast toast = Toast.makeText(getApplicationContext(), "Click on FloatingActionButton to Load JSON", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View view) {
+
+                /**
+                 * Checking Internet Connection
+                 */
+                if (InternetConnection.checkConnection(getApplicationContext())) {
+                    new GetDataTask().execute();
+                } else {
+                    Snackbar.make(view, "Internet Connection Not Available", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Creating Get Data Task for Getting Data From Web
+     */
+    class GetDataTask extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog dialog;
+        int jIndex;
+        int x;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+
+            x=list.size();
+
+            if(x==0)
+                jIndex=0;
+            else
+                jIndex=x;
+
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setTitle("Hey Wait Please..."+x);
+            dialog.setMessage("I am getting your JSON");
+            dialog.show();
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            /**
+             * Getting JSON Object from Web Using okHttp
+             */
+            JSONObject jsonObject = JSONParser.getDataFromWeb();
+
+            try {
+                /**
+                 * Check Whether Its NULL???
+                 */
+                if (jsonObject != null) {
+                    /**
+                     * Check Length...
+                     */
+                    if(jsonObject.length() > 0) {
+                        /**
+                         * Getting Array named "contacts" From MAIN Json Object
+                         */
+                        JSONArray array = jsonObject.getJSONArray(Keys.KEY_CONTACTS);
+
+                        /**
+                         * Check Length of Array...
+                         */
+
+
+                        int lenArray = array.length();
+                        if(lenArray > 0) {
+                            for( ; jIndex < lenArray; jIndex++) {
+
+                                /**
+                                 * Creating Every time New Object
+                                 * and
+                                 * Adding into List
+                                 */
+                                //MyDataModel model = new MyDataModel();
+
+                                /**
+                                 * Getting Inner Object from contacts array...
+                                 * and
+                                 * From that We will get Name of that Contact
+                                 *
+                                 */
+                                JSONObject innerObject = array.getJSONObject(jIndex);
+                                String liist = innerObject.getString(Keys.KEY_NAME);
+                                String checked = innerObject.getString(Keys.KEY_CHECKED);
+
+                                /**
+                                 * Getting Object from Object "phone"
+                                 */
+                                //JSONObject phoneObject = innerObject.getJSONObject(Keys.KEY_PHONE);
+                                //String phone = phoneObject.getString(Keys.KEY_MOBILE);
+
+                                /*model.setName(name);
+                                model.setCountry(country);*/
+
+                                /**
+                                 * Adding name and phone concatenation in List...
+                                 */
+                                list.add(liist+checked);
+                            }
+                        }
+                    }
+                } else {
+
+                }
+            } catch (JSONException je) {
+                Log.i(JSONParser.TAG, "" + je.getLocalizedMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dialog.dismiss();
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if(list.size() > 0) {
+                adapter.notifyDataSetChanged();
+            } else {
+                Snackbar.make(findViewById(R.id.parentLayout), "No Data Found", Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+    class SendRequest extends AsyncTask<String, Void, String> {
+
+
+        protected void onPreExecute(){}
+
+        protected String doInBackground(String... arg0) {
+
+            try{
+
+                URL url = new URL("https://script.google.com/macros/s/AKfycbwl8JZ9LFeCTUA4Wlef2ag5x1ACXT_Aga2giiSYCaoTEqe4Us1K/exec");
+                // https://script.google.com/macros/s/AKfycbyuAu6jWNYMiWt9X5yp63-hypxQPlg5JS8NimN6GEGmdKZcIFh0/exec
+                JSONObject postDataParams = new JSONObject();
+
+                //int i;
+                //for(i=1;i<=70;i++)
+
+
+                //    String usn = Integer.toString(i);
+
+                String id= "1KMWDYyLiaNCHlXQ_ES-rJvG4p0PKqq0Lz0sH6vqYqo4";
+
+                postDataParams.put("checked",checked);
+                postDataParams.put("random",checked);
+                postDataParams.put("id",id);
+
+
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result,
+                    Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+}
+
---
+++ app/src/main/java/com/example/ajaybati/test/util/Keys.java
@@ -0,0 +1,11 @@
+package com.example.ajaybati.test.util;
+
+public class Keys {
+
+    public static final String KEY_CONTACTS = "Sheet1";
+    public static final String KEY_NAME = "list";
+    public static final String KEY_COUNTRY = "specific";
+    public static final String KEY_CHECKED = "checked";
+
+
+}
